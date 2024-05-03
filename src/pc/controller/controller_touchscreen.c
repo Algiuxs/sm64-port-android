922b817479c31d3ca8907919ad0c31c8dea54455
Algiuxs <146809287+Algiuxs@users.noreply.github.com>
2024-05-03 20:49:23 +0300

    Revert "Update controller_touchscreen.c"
    This reverts commit 4d38375ab16fca6fa42c88faa61c43fe0599506b.

--- src/pc/controller/controller_touchscreen.c
+++ src/pc/controller/controller_touchscreen.c
@@ -1,32 +1,39 @@
 //Feel free to use it in your port too, but please keep authorship!
 //Touch Controls made by: VDavid003
 #ifdef TOUCH_CONTROLS
-#include <ultra64.h>
 #include <PR/ultratypes.h>
 #include <PR/gbi.h>
+
 #include "config.h"
-#include "sm64.h"
 #include "game/game_init.h"
 #include "game/memory.h"
 #include "game/segment2.h"
 #include "gfx_dimensions.h"
 #include "pc/gfx/gfx_pc.h"
+
 #include "controller_api.h"
 #include "controller_touchscreen.h"
+
 #define SCREEN_WIDTH_API 1280
 #define SCREEN_HEIGHT_API 960
+
 #define LEFT_EDGE ((int)floorf(SCREEN_WIDTH_API / 2 - SCREEN_HEIGHT_API / 2 * gfx_current_dimensions.aspect_ratio))
 #define RIGHT_EDGE ((int)ceilf(SCREEN_WIDTH_API / 2 + SCREEN_HEIGHT_API / 2 * gfx_current_dimensions.aspect_ratio))
+
 #define CORRECT_TOUCH_X(x) ((x * (RIGHT_EDGE - LEFT_EDGE)) + LEFT_EDGE)
 #define CORRECT_TOUCH_Y(y) (y * SCREEN_HEIGHT_API)
+
 #define JOYSTICK_SIZE 280
+
 enum ControlElementType {
     Joystick,
     Button
 };
+
 struct Position {
     s32 x,y;
 };
+
 struct ControlElement {
     enum ControlElementType type;
     struct Position (*GetPos)();
@@ -38,14 +45,16 @@ struct ControlElement {
     char character;
     int slideTouch;
 };
+
 #include "controller_touchscreen_layouts.inc"
+
 static struct ControlElement *ControlElements = ControlElementsDefault;
 static int ControlElementsLength = sizeof(ControlElementsDefault)/sizeof(struct ControlElement);
+
 #define TRIGGER_DETECT(size) (((pos.x + size / 2 > CORRECT_TOUCH_X(event->x)) && (pos.x - size / 2 < CORRECT_TOUCH_X(event->x))) &&\
                               ((pos.y + size / 2 > CORRECT_TOUCH_Y(event->y)) && (pos.y - size / 2 < CORRECT_TOUCH_Y(event->y))))
 
 void touch_down(struct TouchEvent* event) {
-    set_current_input(touchScreen);
     struct Position pos;
     for(int i = 0; i < ControlElementsLength; i++) {
         if (ControlElements[i].touchID == 0) {
@@ -67,6 +76,7 @@ void touch_down(struct TouchEvent* event) {
         }
     }
 }
+
 void touch_motion(struct TouchEvent* event) {
     struct Position pos;
     for(int i = 0; i < ControlElementsLength; i++) {
@@ -87,6 +97,7 @@ void touch_motion(struct TouchEvent* event) {
                             y = JOYSTICK_SIZE/2;
                         if (pos.y - JOYSTICK_SIZE/2 > CORRECT_TOUCH_Y(event->y))
                             y = -JOYSTICK_SIZE/2;
+
                         ControlElements[i].joyX = x;
                         ControlElements[i].joyY = y;
                         break;
@@ -112,6 +123,7 @@ void touch_motion(struct TouchEvent* event) {
         }
     }
 }
+
 static void handle_touch_up(int i) {//seperated for when the layout changes
     ControlElements[i].touchID = 0;
     switch (ControlElements[i].type) {
@@ -123,6 +135,7 @@ static void handle_touch_up(int i) {//seperated for when the layout changes
             break;
     }
 }
+
 void touch_up(struct TouchEvent* event) {
     struct Position pos;
     for(int i = 0; i < ControlElementsLength; i++) {
@@ -131,15 +144,20 @@ void touch_up(struct TouchEvent* event) {
         }
     }
 }
+
 ALIGNED8 static const u8 texture_button[] = {
 #include "textures/touchcontrols/touch_button.rgba16.inc.c"
 };
+
 ALIGNED8 static const u8 texture_button_dark[] = {
 #include "textures/touchcontrols/touch_button_dark.rgba16.inc.c"
 };
+
 //Sprite drawing code stolen from src/game/print.c
+
 static void select_button_texture(int dark) {
     const u8 *const *glyphs = segmented_to_virtual(main_hud_lut);
+
     gDPPipeSync(gDisplayListHead++);
     if (!dark) {
         gDPSetTextureImage(gDisplayListHead++, G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, texture_button);
@@ -149,31 +167,34 @@ static void select_button_texture(int dark) {
     }
     gSPDisplayList(gDisplayListHead++, dl_hud_img_load_tex_block);
 }
+
 static void select_char_texture(char num) {
     const u8 *const *glyphs = segmented_to_virtual(main_hud_lut);
+
     gDPPipeSync(gDisplayListHead++);
     gDPSetTextureImage(gDisplayListHead++, G_IM_FMT_RGBA, G_IM_SIZ_16b, 1, glyphs[num - 87]);
     gSPDisplayList(gDisplayListHead++, dl_hud_img_load_tex_block);
 }
+
 static void DrawSprite(s32 x, s32 y, int scaling) {
     gSPTextureRectangle(gDisplayListHead++, x - (15 << scaling), y - (15 << scaling), x + (15 << scaling),
                         y + (15 << scaling), G_TX_RENDERTILE, 0, 0, 4 << (11 - scaling), 1 << (11 - scaling));
 }
 
 void render_touch_controls(void) {
-    if(get_current_input()!=touchScreen)
-        return;
-
     Mtx *mtx;
 
     mtx = alloc_display_list(sizeof(*mtx));
+
     if (mtx == NULL) {
         return;
     }
+
     guOrtho(mtx, 0.0f, SCREEN_WIDTH, 0.0f, SCREEN_HEIGHT, -10.0f, 10.0f, 1.0f);
     gSPPerspNormalize((Gfx *) (gDisplayListHead++), 0xFFFF);
     gSPMatrix(gDisplayListHead++, VIRTUAL_TO_PHYSICAL(mtx), G_MTX_PROJECTION | G_MTX_LOAD | G_MTX_NOPUSH);
     gSPDisplayList(gDisplayListHead++, dl_hud_img_begin);
+
     struct Position pos;
     for (int i = 0; i < ControlElementsLength; i++) {
         select_button_texture(0);
@@ -193,10 +214,13 @@ void render_touch_controls(void) {
                 break;
         }
     }
+
     gSPDisplayList(gDisplayListHead++, dl_hud_img_end);
 }
-static void touchscreen_init(void) {
+
+static void touchscreen_init() {
 }
+
 static void touchscreen_read(OSContPad *pad) {
     for(int i = 0; i < ControlElementsLength; i++) {
         switch (ControlElements[i].type) {
@@ -214,17 +238,8 @@ static void touchscreen_read(OSContPad *pad) {
         }
     }
 }
-static u32 touchscreen_rawkey(void) { //dunno what this does but I'll skip it for now
-    return VK_INVALID;
-}
 struct ControllerAPI controller_touchscreen = {
-    0,
     touchscreen_init,
-    touchscreen_read,
-    touchscreen_rawkey,
-    NULL,
-    NULL,
-    NULL,
-    NULL
+    touchscreen_read
 };
 #endif
