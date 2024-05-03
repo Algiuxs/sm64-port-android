684a737b5fa94f594eb2aa9424f798954da4be48
Algiuxs <146809287+Algiuxs@users.noreply.github.com>
2024-05-03 20:31:15 +0300

    Update controller_api.h

--- src/pc/controller/controller_api.h
+++ src/pc/controller/controller_api.h
@@ -1,11 +1,21 @@
 #ifndef CONTROLLER_API
 #define CONTROLLER_API
-
 #include <ultra64.h>
-
 struct ControllerAPI {
     void (*init)(void);
     void (*read)(OSContPad *pad);
 };
+
+enum Input{
+    keyboard,
+    emscriptenKeyboard,
+    sdlGameController,
+    touchScreen,
+    wup,
+    xinput
+};
+
+extern void set_current_input(int in);
+extern int get_current_input();
 
 #endif
